e31900ae267491abce4ba548ee6c9b441354f2ee
VarthanV <varthanv26018@gmail.com>
2019-09-10 22:27:08 +0530

    May God bless me

--- stockapp/views.py
+++ stockapp/views.py
@@ -272,4 +272,5 @@ class TestView(APIView):
      def get(self,request):
          process = Popen(['npm'], stdout=PIPE, stderr=PIPE)
          stdout, stderr = process.communicate()
+         print("Workss")
          return Response(stdout)
